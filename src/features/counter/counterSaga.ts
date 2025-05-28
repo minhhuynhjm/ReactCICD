bdf36ea7efd7d34e0306e768ecd541ff7b4370aa
Minh Huynh <minhhuynhjm@gmail.com>
2025-05-28 22:08:11 +0700

    fix bug

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,14 +1,15 @@
 import { PayloadAction } from '@reduxjs/toolkit';
-import { take, takeEvery } from 'redux-saga/effects';
+import { takeEvery } from 'redux-saga/effects'; // Removed unused 'take'
 import { increment } from './counterSlice';
 
 export function* log(action: PayloadAction) {
   console.log('Action:', action);
+  yield; // Add a yield to satisfy require-yield
 }
 
 export default function* counterSaga() {
   console.log('counterSaga');
-  // Add your saga logic here 11
+  // Add your saga logic here
 
   yield takeEvery(increment().type, log);
 }
