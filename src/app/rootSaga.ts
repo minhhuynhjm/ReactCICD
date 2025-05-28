c1a3b3a80ef932214a43d3eba386a54b0c5e69cd
Minh Huynh <minhhuynhjm@gmail.com>
2025-05-28 22:10:43 +0700

    fix bug

--- src/app/rootSaga.ts
+++ src/app/rootSaga.ts
@@ -1,9 +1,9 @@
-
 import counterSaga from 'features/counter/counterSaga';
 import { all } from 'redux-saga/effects';
 
 function* helloSaga() {
   console.log('Hello Sagas!');
+  yield null; // Add a yield to satisfy require-yield
 }
 
 export default function* rootSaga() {
